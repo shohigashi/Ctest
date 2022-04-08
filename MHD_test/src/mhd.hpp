3d7233d40c72b9b9b32d8572c3d90105d3a24119
shohigashi <shigashi.phy@gmail.com>
2022-04-09 00:24:18 +0900

    modified

---
+++ MHD_test/src/mhd.hpp
@@ -0,0 +1,17 @@
+#ifndef MHD_HH
+#define MHD_HH
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <tuple>
+#include <cmath>
+
+// grid number 
+constexpr size_t XN = 200, YN = 200;
+// number of file output
+constexpr size_t PN = 100;
+// number of variables
+constexpr size_t VN = 9;
+// 
